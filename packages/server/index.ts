850ac967a64a64f0b55aa675d6a4933ac3ee5522
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-01 23:59:47 +0200

    Updated to yarn init -2.

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,9 @@
-console.log("Server running");
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Image Gallery listening on port 3000!");
+});
+
+// console.log("Listening!");
