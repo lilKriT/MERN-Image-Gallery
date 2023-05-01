7f06ec1e3b8a8c8f380418a3a1dac06daee0d33a
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-02 01:13:57 +0200

    Added some tasks. Added dotenv. Added colors.

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,7 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
 
 const app = express();
+dotenv.config({ path: "../../.env" });
+const port = process.env.PORT || 5555;
 
-app.listen(3000, () => {
-  console.log("Image Gallery listening on port 3000!");
+app.listen(port, () => {
+  console.log(
+    colors.green.underline(`Image Gallery listening on port ${port}`)
+  );
 });
+
+// TODO: router for users
+// TODO: router for images
+
+// TODO: connect to db
+
+// TODO: middleware for password
+// TOOD: middleware for errors
