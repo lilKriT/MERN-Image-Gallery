2d32fb23ffc1822f3b988a35a871a99824bcfae7
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-02 23:56:46 +0200

    Updated yarn.lock Created a user model.

--- packages/server/models/User.ts
+++ packages/server/models/User.ts
@@ -1,12 +1,26 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please add a user name"],
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a user name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add a username"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Please add a username"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
