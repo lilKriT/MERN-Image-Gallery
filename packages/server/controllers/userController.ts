cc18eff50a2b367ddd9158c6749b9ba36e60b262
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-03 22:19:58 +0200

    Added jsdocs for user.

--- packages/server/controllers/userController.ts
+++ packages/server/controllers/userController.ts
@@ -1,11 +1,20 @@
+// @desc Register user
+// @route POST api/v1/users
+// @access Public
 const registerUser = () => {
   console.log("Registering user");
 };
 
+// @desc Log in user
+// @route POST api/v1/users/login
+// @access Public
 const loginUser = () => {
   console.log("User Logged in");
 };
 
+// @desc Get user info
+// @route POST api/v1/users/me
+// @access Private
 const getMe = () => {
   console.log("My info");
 };
