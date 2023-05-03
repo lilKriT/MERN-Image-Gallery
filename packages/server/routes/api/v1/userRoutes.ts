12c34c44aa0fa88c0f59f25debc2c561507a6cb4
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-03 22:23:04 +0200

    Created a user router.

--- packages/server/routes/api/v1/userRoutes.ts
+++ packages/server/routes/api/v1/userRoutes.ts
@@ -1,1 +1,13 @@
 import { Router } from "express";
+import {
+  registerUser,
+  loginUser,
+  getMe,
+} from "../../../controllers/userController";
+
+const userRoutes = Router();
+userRoutes.post("/", registerUser);
+userRoutes.post("login", loginUser);
+userRoutes.get("/me", getMe);
+
+export default userRoutes;
