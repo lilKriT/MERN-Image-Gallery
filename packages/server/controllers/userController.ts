509637bb04e29968f4e4f08cbdc2818e635c23d0
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-03 22:18:02 +0200

    Starting user controller.

---
+++ packages/server/controllers/userController.ts
@@ -0,0 +1,13 @@
+const registerUser = () => {
+  console.log("Registering user");
+};
+
+const loginUser = () => {
+  console.log("User Logged in");
+};
+
+const getMe = () => {
+  console.log("My info");
+};
+
+export { registerUser, loginUser, getMe };
