57d3b1a0525ff6d51fd3c777c5f176a3ab009f57
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-04 23:26:24 +0200

    Added a user router.

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,11 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import userRoutes from "./routes/api/v1/userRoutes";
 
+// Basic config
 const app = express();
 dotenv.config({ path: "../../.env" });
 const port = process.env.PORT || 3000;
 
+// Middleware
+
+// Routes
+const router = express.Router();
+router.use("/api/v1/users", userRoutes);
+
+app.use(router);
+
+// Listening
 app.listen(port, () => {
   console.log(
     colors.green.underline(`Image Gallery listening on port ${port}`)
