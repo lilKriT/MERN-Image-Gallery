8e886830f6f9fe1b3f726baa08e02ba4de7911e1
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-04 23:57:55 +0200

    Working on db connection.

--- packages/server/config/db.ts
+++ packages/server/config/db.ts
@@ -1,6 +1,17 @@
 import mongoose from "mongoose";
 import colors from "colors";
 
-const connectDB = () => {};
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI as string);
+
+    console.log(
+      colors.cyan.underline(`MongoDB Connected: ${conn.connection.host}`)
+    );
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
