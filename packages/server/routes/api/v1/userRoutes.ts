f5709f19ff500090c2d2d814eb17953576cd9aaf
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-06 01:16:09 +0200

    User info is password protected.

--- packages/server/routes/api/v1/userRoutes.ts
+++ packages/server/routes/api/v1/userRoutes.ts
@@ -4,10 +4,11 @@ import {
   loginUser,
   getMe,
 } from "../../../controllers/userController";
+import protect from "../../../middleware/authMiddleware";
 
 const userRoutes = Router();
 userRoutes.post("/", registerUser);
 userRoutes.post("/login", loginUser);
-userRoutes.get("/me", getMe);
+userRoutes.get("/me", protect, getMe);
 
 export default userRoutes;
