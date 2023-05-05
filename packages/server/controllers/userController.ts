d5b0773004b7bef5ce1125a2ceba36e0c77b5412
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-05 23:13:00 +0200

    Added express async handler.

--- packages/server/controllers/userController.ts
+++ packages/server/controllers/userController.ts
@@ -1,27 +1,30 @@
 import { Request, Response } from "express";
+import expressAsyncHandler from "express-async-handler";
 
 // @desc Register user
 // @route POST api/v1/users
 // @access Public
-const registerUser = (req: Request, res: Response) => {
-  console.log("Registering user");
-  res.status(200).send("Registering user");
-};
+const registerUser = expressAsyncHandler(
+  async (req: Request, res: Response) => {
+    console.log("Registering user");
+    res.status(200).send("Registering user");
+  }
+);
 
 // @desc Log in user
 // @route POST api/v1/users/login
 // @access Public
-const loginUser = (req: Request, res: Response) => {
+const loginUser = expressAsyncHandler(async (req: Request, res: Response) => {
   console.log("User Logged in");
   res.status(200).send("Logging user in");
-};
+});
 
 // @desc Get user info
 // @route POST api/v1/users/me
 // @access Private
-const getMe = (req: Request, res: Response) => {
+const getMe = expressAsyncHandler(async (req: Request, res: Response) => {
   console.log("My info");
   res.status(200).send("User information");
-};
+});
 
 export { registerUser, loginUser, getMe };
