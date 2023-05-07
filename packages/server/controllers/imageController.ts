6d9e5b101f304dc7e5e9fc5b28e896c11054fc28
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-07 23:26:00 +0200

    Wrapped image methods in async handler.

--- packages/server/controllers/imageController.ts
+++ packages/server/controllers/imageController.ts
@@ -5,22 +5,22 @@ import Image from "../models/Image";
 // @desc Get Image
 // @route GET api/v1/image
 // @access Public
-const getImage = async (req: Request, res: Response) => {
+const getImage = expressAsyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ msg: "Getting image" });
-};
+});
 
 // @desc Add Image
 // @route POST api/v1/image
 // @access Private
-const addImage = async (req: Request, res: Response) => {
+const addImage = expressAsyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ msg: "Adding image" });
-};
+});
 
 // @desc Remove Image
 // @route DELETE api/v1/image
 // @access Private
-const deleteImage = async (req: Request, res: Response) => {
+const deleteImage = expressAsyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ msg: "Removing image" });
-};
+});
 
 export { getImage, addImage, deleteImage as removeImage };
