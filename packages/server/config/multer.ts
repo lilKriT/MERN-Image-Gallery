c58b30e7ca7c9c45e8e8dd49da964a64f46b8382
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-08 00:08:44 +0200

    Moved multer to a separate file.

---
+++ packages/server/config/multer.ts
@@ -0,0 +1,47 @@
+import multer from "multer";
+
+const storageEngine = multer.diskStorage({
+  destination: "./images", // Is this correct still?
+  filename: (req, file, cb) => {
+    cb(null, `${file.originalname}`);
+  },
+});
+
+const upload = multer({
+  storage: storageEngine,
+  limits: {
+    fileSize: 1000000, // current limit: 1MB
+  },
+  // fileFilter: (req, file, cb) => {
+  //   checkImageType(file, cb);
+  // },
+});
+
+// // Testing for file types
+// import path from "path";
+// // I should fix the typing here
+// const checkImageType = (file: any, cb: any) => {
+//   // Allowed extensions
+//   const fileTypes = /jpeg|jpg|png|gif|svg/;
+
+//   // Check extension names
+//   const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+
+//   const mimeType = fileTypes.test(file.mimeType);
+
+//   if (mimeType && extName) {
+//     return cb(null, true);
+//   } else {
+//     cb("You can only upload images!");
+//   }
+// };
+
+// Adding image uploads
+// router.post("/api/v1/image/single", upload.single("image"), (req, res) => {
+//   console.log("Bang");
+//   if (req.file) {
+//     res.send("Single file uploaded");
+//   } else {
+//     res.status(400).send("Please send a valid image");
+//   }
+// });
