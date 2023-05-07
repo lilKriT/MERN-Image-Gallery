e3bdc4d9f8ad4953811b1a508206f1e9a29a3680
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-07 23:07:32 +0200

    Added simple image methods. Added JS docs.

--- packages/server/controllers/imageController.ts
+++ packages/server/controllers/imageController.ts
@@ -1,7 +1,20 @@
+// @desc Get Image
+// @route GET api/v1/image
+// @access Public
+const getImage = () => {
+  console.log("Getting image");
+};
+
+// @desc Add Image
+// @route POST api/v1/image
+// @access Private
 const addImage = () => {
   console.log("Adding image");
 };
 
+// @desc Remove Image
+// @route DELETE api/v1/image
+// @access Private
 const removeImage = () => {
   console.log("Removing image");
 };
