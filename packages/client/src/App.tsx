c4e5aea3a6f3b7ea12ed5071a78f7bcb45b76d05
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-09 00:45:57 +0200

    Basic styling.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
+  const user = false;
+
   return (
     <>
       <Router>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={user ? <Home /> : <Login />} />
+          <Route path="/register" element={user ? <Home /> : <Register />} />
         </Routes>
       </Router>
     </>
--- packages/client/src/components/Header.tsx
+++ packages/client/src/components/Header.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="flex justify-center">
+      <div className="container flex justify-between items-center mt-4">
+        <Link to="/">LOGO</Link>
+        <nav>
+          <menu className="flex gap-4">
+            <li>
+              <Link to="/login" className="btn btn--primary">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register" className="btn btn--secondary">
+                Register
+              </Link>
+            </li>
+          </menu>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
