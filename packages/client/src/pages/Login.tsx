44fbdfdebc93d760f2914e4b143b2b96b0432cdc
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-09 01:12:09 +0200

    Working on login page.

--- packages/client/src/pages/Login.tsx
+++ packages/client/src/pages/Login.tsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(`Login: ${login}, password: ${password}`);
+  };
+
   return (
     <div className="flex justify-center">
       <div className="container">
-        <form className="flex flex-col gap-4 shadow-lg shadow-black/30 p-8 rounded-3xl">
+        <form
+          className="flex flex-col gap-4 shadow-lg shadow-black/30 p-8 rounded-3xl"
+          onSubmit={(e) => handleSubmit(e)}
+        >
           <h2>Insert your login and password</h2>
-          <label className="flex flex-col">
+          <label className="flex flex-col gap-2">
             Login:
-            <input type="text" className="border-b border-black" />
+            <input
+              type="text"
+              className="inputField"
+              placeholder="Your name"
+              value={login}
+              onChange={(e) => setLogin(e.target.value)}
+            />
           </label>
-          <label className="flex flex-col">
+          <label className="flex flex-col gap-2">
             Password:
-            <input type="password" className="border-b border-black" />
+            <input
+              type="password"
+              className="inputField"
+              placeholder="Your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </label>
-          <button className="btn btn--primary">Log In</button>
+          <button className="btn btn--primary">Sign In</button>
         </form>
       </div>
     </div>
