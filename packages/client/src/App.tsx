fd65af1016dd7102e21d6c1e1a823c9e280cec99
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-09 00:19:22 +0200

    Added a basic router and home page.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
-      <p className="text-blue-700 font-lato">Tailwind installed!</p>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
