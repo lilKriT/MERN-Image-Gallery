3e46b64e80db7027a60fa7cf8e1012113768b612
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-09 23:38:10 +0200

    Added a form to post pictures.

--- packages/client/src/pages/NewPic.tsx
+++ packages/client/src/pages/NewPic.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const NewPic = () => {
-  return <div>NewPic</div>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Sending a pic");
+  };
+
+  return (
+    <div className="flex justify-center">
+      <div className="container">
+        Add pics here
+        <form
+          onSubmit={(e) => handleSubmit(e)}
+          className="flex flex-col gap-4 shadow-lg shadow-black/30 p-8 rounded-3xl mt-16"
+        >
+          <label>
+            Insert pic:
+            <input type="file" name="pic" id="pic" />
+            <button className="btn btn--primary">Post picture</button>
+          </label>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default NewPic;
