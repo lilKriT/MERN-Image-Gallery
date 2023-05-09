64ddcfb720fca1339f1ce652b251e3181a4f3f31
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-09 19:49:04 +0200

    Added fetching to context.

--- packages/client/src/context/AppContext.tsx
+++ packages/client/src/context/AppContext.tsx
@@ -5,6 +5,8 @@ interface IContext {
   // text: string;
   user: IUser | null;
   setUser: React.Dispatch<React.SetStateAction<IUser | null>>;
+  isFetching: boolean;
+  setIsFetching: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 interface IProps {
@@ -15,13 +17,16 @@ const AppContext = createContext<IContext>({
   // text: "Default Text",
   user: null,
   setUser: () => {},
+  isFetching: false,
+  setIsFetching: () => {},
 });
 
 const AppProvider = ({ children }: IProps) => {
   const [user, setUser] = useState<IUser | null>(null);
+  const [isFetching, setIsFetching] = useState(false);
 
   return (
-    <AppContext.Provider value={{ user, setUser }}>
+    <AppContext.Provider value={{ user, setUser, isFetching, setIsFetching }}>
       {children}
     </AppContext.Provider>
   );
