1b21cbae010062cf98954f37e996e6eee7865b66
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-09 20:09:25 +0200

    You can log in.

--- packages/client/src/pages/Login.tsx
+++ packages/client/src/pages/Login.tsx
@@ -1,16 +1,38 @@
 import React, { useContext, useState } from "react";
-// import axios from "axios";
+import axios from "axios";
 import { AppContext } from "../context/AppContext";
 
+const url = "http://localhost:3000/api/v1";
+
 const Login = () => {
-  const [login, setLogin] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const { user, setUser } = useContext(AppContext);
+  const { user, setUser, isFetching, setIsFetching } = useContext(AppContext);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(`Login: ${login}, password: ${password}`);
+    // console.log(`Login: ${login}, password: ${password}`);
+
+    try {
+      // Log In Start
+      setUser(null);
+      setIsFetching(true);
+
+      const res = await axios.post(`${url}/users/login`, {
+        email,
+        password,
+      });
+
+      console.log(res.data);
+      // Log In Success
+      setUser(res.data);
+      setIsFetching(false);
+    } catch (err) {
+      // Log In Fail
+      setUser(null);
+      setIsFetching(false);
+    }
   };
 
   return (
@@ -20,16 +42,16 @@ const Login = () => {
           className="flex flex-col gap-4 shadow-lg shadow-black/30 p-8 rounded-3xl mt-16"
           onSubmit={(e) => handleSubmit(e)}
         >
-          <h2>Insert your login and password</h2>
+          <h2>Insert your email and password</h2>
           <h3>{user ? user.name : "no user"}</h3>
           <label className="flex flex-col gap-2">
-            Login:
+            Email:
             <input
               type="text"
               className="inputField"
               placeholder="Your name"
-              value={login}
-              onChange={(e) => setLogin(e.target.value)}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </label>
           <label className="flex flex-col gap-2">
