f372139eb2ddf9c5fe97fef11d8296ae8560f029
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-09 18:56:45 +0200

    Started using context.

--- packages/client/src/pages/Login.tsx
+++ packages/client/src/pages/Login.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 // import axios from "axios";
+import { AppContext } from "../context/AppContext";
 
 const Login = () => {
   const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
+
+  const { text } = useContext(AppContext);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -14,10 +17,11 @@ const Login = () => {
     <div className="flex justify-center">
       <div className="container">
         <form
-          className="flex flex-col gap-4 shadow-lg shadow-black/30 p-8 rounded-3xl"
+          className="flex flex-col gap-4 shadow-lg shadow-black/30 p-8 rounded-3xl mt-16"
           onSubmit={(e) => handleSubmit(e)}
         >
           <h2>Insert your login and password</h2>
+          <h3>{text}</h3>
           <label className="flex flex-col gap-2">
             Login:
             <input
