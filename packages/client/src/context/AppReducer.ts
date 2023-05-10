46a2486df5140f44f51d3b28f22de9a676449520
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-10 23:03:55 +0200

    Working on Reducer.

--- packages/client/src/context/AppReducer.ts
+++ packages/client/src/context/AppReducer.ts
@@ -1,8 +1,28 @@
-// There's two main ways of making reducers, which should I use?
-// Actions vs Partial State
-const AppReducer = (state: any, action: any) => {
-  switch (action.type) {
+enum TempActionType {
+  login = "login",
+  logout = "logout",
+}
+
+interface TempAction {
+  type: TempActionType;
+  payload: string;
+}
+
+interface TempState {
+  text: "bla bla";
+}
+
+const AppReducer = (state: TempState, action: TempAction) => {
+  const { type, payload } = action;
+  switch (type) {
     case "login":
       console.log("reducer login");
+      return "abc";
+    case "logout":
+      console.log("reducer logout");
+      return "cda";
+    default:
+      console.log("reducer default");
+      throw new Error(`Unknown action: ${type}`);
   }
 };
